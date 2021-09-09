d76eb028e039ecdba49ceaf710d80c5bf9ef06dc
Sarah Jane West <4419364+sarad1p1ty@users.noreply.github.com>
2021-09-09 19:11:54 +0100

    Exercise 2 - extra credits 3 & 4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,19 +3,31 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => JSON.parse(window.localStorage.getItem(key)) || defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState((() => window.localStorage.getItem('name')) || initialName)
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-  React.useEffect(() => window.localStorage.setItem('name', name), [name])
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form autocomplete="off">
